908805a820634e7f4df41f0bc2d31d215958cc3b
regis-zang <celregiszang@gmail.com>
2024-05-19 08:28:37 -0300

    new file

---
+++ Novo Arquivo.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Script: temp.py
+Descrição: Este script faz XYZ.
+Autor: Regis Zang
+Data: DD/MM/AAAA
+Versão: 1.0
+Licença: MIT
+Requisitos: biblioteca_x, biblioteca_y
+"""
+
+a = 10
+
+print("hello")
+
